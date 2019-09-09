0494c24b01a8847190ce86d76c9ecef758a885a7
qiangzhouliang <2538096489@qq.com>
2019-09-09 22:05:02 +0800

    init porject

---
+++ src/components/Pos.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="pos">
+   Hello AwesomePos demo
+ </div>
+</template>
+
+<script>
+export default {
+ name: "pos",
+ data() {
+  return {
+
+  }
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style scoped>
+
+</style>
